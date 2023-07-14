4c29c3b553aeab77bc2289678e17d743c2dd0cdd
arun1493 <arunkumarvcet1@gmail.com>
2023-07-14 23:56:31 +0530

    WIP

--- apps/web/src/speechToText.ts
+++ apps/web/src/speechToText.ts
@@ -24,16 +24,17 @@ const transcribe = (audioBytes) => {
     console.log(audioBuffer)
     // console.log(typeof audioBytes, audioBytes.toString('base64'))
     // Reads a local audio file and converts it to base64
-    // const audio = {
-    //     content: audioBuffer.toString('base64'),
-    // };
+    const audio = {
+        content: audioBuffer.toString('base64'),
+    };
 
-    fs.writeFile('demo.wav', Buffer.concat([audioBuffer]), (err) => {console.log(err)})
+    // fs.writeFile('demo.wav', Buffer.concat([audioBuffer]), (err) => {console.log(err)})
     // The audio file's encoding, sample rate in hertz, and BCP-47 language code
     const config = {
         encoding: 'LINEAR16',
         // sampleRateHertz: 48000,
         languageCode: 'en-US',
+        audioChannelCount: 2,
         
     };
 
@@ -43,20 +44,20 @@ const transcribe = (audioBytes) => {
     };
 
     // Detects speech in the audio file
-    // return speechClient
-    //     .recognize(request)
-    //     .then((data) => {
-    //         console.log(data)
-    //         const results = _.get(data[0], 'results', []);
-    //         console.log(results)
-    //         const transcription = results
-    //             .map(result => result.alternatives[0].transcript)
-    //             .join('\n');
-    //         console.log(`Transcription: ${transcription}`);
-    //     })
-    //     .catch(err => {
-    //         console.error('ERROR:', err);
-    //     });
+    return speechClient
+        .recognize(request)
+        .then((data) => {
+            console.log(data)
+            const results = _.get(data[0], 'results', []);
+            console.log(results)
+            const transcription = results
+                .map(result => result.alternatives[0].transcript)
+                .join('\n');
+            console.log(`Transcription: ${transcription}`);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
 }
 
 export default transcribe
